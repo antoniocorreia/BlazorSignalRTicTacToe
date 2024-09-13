871fce122897c53869ae2d4cb438bd6c4e5e5c2a
Antonio Correia <antonio.alves.correia@gmail.com>
2024-09-13 01:48:20 -0300

    First version of the app

---
+++ BlazorTicTacToe/BlazorTicTacToe.Shared/TicTacToeGame.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorTicTacToe.Shared
+{
+    public class TicTacToeGame
+    {
+        public string PlayerXId { get; set; }
+        public string PlayerOId { get; set; }
+    }
+}
---
+++ BlazorTicTacToe/BlazorTicTacToe/BlazorTicTacToe/Hubs/GameHub.cs
@@ -0,0 +1,50 @@
+﻿using BlazorTicTacToe.Shared;
+using Microsoft.AspNetCore.SignalR;
+
+namespace BlazorTicTacToe.Hubs
+{
+    public class GameHub : Hub
+    {
+        private static readonly List<GameRoom> _rooms = new();
+        public override async Task OnConnectedAsync()
+        {
+            Console.WriteLine($"Player with Id '{Context.ConnectionId}' connected.");
+            
+            await Clients.Caller.SendAsync("Rooms", _rooms.OrderBy(r => r.RoomName));
+        }
+
+        public async Task<GameRoom> CreateRoom(string name, string playerName)
+        {
+            var roomId = Guid.NewGuid().ToString();
+            var room = new GameRoom(roomId, name);
+            _rooms.Add(room);
+
+            var newPlayer = new Player(Context.ConnectionId, playerName);
+            room.TryAddPlayer(newPlayer);
+
+            await Groups.AddToGroupAsync(Context.ConnectionId, roomId);
+            await Clients.All.SendAsync("Rooms", _rooms.OrderBy(r => r.RoomName));
+
+            return room;
+        }
+
+        public async Task<GameRoom?> JoinRoom(string roomId, string playerName)
+        {
+            var room = _rooms.FirstOrDefault(r => r.RoomId == roomId);
+
+            if (room is not null)
+            {
+                var newPlayer = new Player(Context.ConnectionId, playerName);
+                if (room.TryAddPlayer(newPlayer))
+                {
+                    await Groups.AddToGroupAsync(Context.ConnectionId, roomId);
+                    await Clients.Group(roomId).SendAsync("PlayerJoined", newPlayer);
+
+                    return room;
+                }
+            }
+            
+            return null;
+        }
+    }
+}
